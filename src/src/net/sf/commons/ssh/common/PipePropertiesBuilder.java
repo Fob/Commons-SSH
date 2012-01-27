4ae436192cc2e06382c4566d5f96e2b86e136d71
Alexey Polbitsyn <Fob.0xFF@gmail.com>
2012-01-27 16:28:09 +0400

    continue sshd implementation

--- src/src/net/sf/commons/ssh/common/PipePropertiesBuilder.java
+++ src/src/net/sf/commons/ssh/common/PipePropertiesBuilder.java
@@ -1,8 +1,6 @@
 package net.sf.commons.ssh.common;
 
-import net.sf.commons.ssh.options.Properties;
-import net.sf.commons.ssh.options.PropertiesBuilder;
-import net.sf.commons.ssh.options.PropertyType;
+import net.sf.commons.ssh.options.*;
 
 public class PipePropertiesBuilder extends PropertiesBuilder
 {
@@ -21,44 +19,95 @@ public class PipePropertiesBuilder extends PropertiesBuilder
 
     protected PipePropertiesBuilder()
     {
-        defaultProperties.put(INITIAL_SIZE,1024);
-        defaultProperties.put(MAXIMUM_SIZE,2*1024*1024);
-        defaultProperties.put(STEP_SIZE,1024);
-        defaultProperties.put(MODIFIER_SIZE,2);
-        defaultProperties.put(ALLOCATOR,new SoftBufferAllocator());
+        defaultProperties.put(INITIAL_SIZE, 1024);
+        defaultProperties.put(MAXIMUM_SIZE, 2 * 1024 * 1024);
+        defaultProperties.put(STEP_SIZE, 1024);
+        defaultProperties.put(MODIFIER_SIZE, 2);
+        defaultProperties.put(ALLOCATOR, new SoftBufferAllocator());
     }
 
     public synchronized static PipePropertiesBuilder getInstance()
-	{
-		if (instance == null)
-		{
-			instance = new PipePropertiesBuilder();
-		}
-		return instance;
-	}
+    {
+        if (instance == null)
+        {
+            instance = new PipePropertiesBuilder();
+        }
+        return instance;
+    }
 
     public Integer getInitialSize(Properties conf)
     {
-        return (Integer) getProperty(conf,INITIAL_SIZE);
+        return (Integer) getProperty(conf, INITIAL_SIZE);
     }
 
     public Integer getMaximumSize(Properties conf)
     {
-        return (Integer) getProperty(conf,MAXIMUM_SIZE);
+        return (Integer) getProperty(conf, MAXIMUM_SIZE);
     }
 
     public Integer getStepSize(Properties conf)
     {
-        return (Integer) getProperty(conf,STEP_SIZE);
+        return (Integer) getProperty(conf, STEP_SIZE);
     }
 
     public Integer getModifier(Properties conf)
     {
-        return (Integer) getProperty(conf,MODIFIER_SIZE);
+        return (Integer) getProperty(conf, MODIFIER_SIZE);
     }
 
-    public BufferAllocator getAllocator(Properties conf)
+    public BufferAllocator etAllocator(Properties conf)
     {
-        return (BufferAllocator) getProperty(conf,ALLOCATOR);
+        return (BufferAllocator) getProperty(conf, ALLOCATOR);
+    }
+
+
+    public void setInitialSize(Configurable conf, int size)
+    {
+        setProperty(conf, INITIAL_SIZE, size);
+    }
+
+    public void setMaximumSize(Configurable conf, int size)
+    {
+        setProperty(conf, MAXIMUM_SIZE, size);
+    }
+
+    public void setStepSize(Configurable conf, int size)
+    {
+        setProperty(conf, STEP_SIZE, size);
+    }
+
+    public void setModifier(Configurable conf, int size)
+    {
+        setProperty(conf, MODIFIER_SIZE, size);
+    }
+
+    public void setAllocator(Configurable conf, BufferAllocator allocator)
+    {
+        setProperty(conf, ALLOCATOR, allocator);
+    }
+
+    public void setAllocator(Configurable conf, String allocator)
+    {
+        setProperty(conf, ALLOCATOR, allocator);
+    }
+
+    @Override
+    protected TypeConverter createConverter()
+    {
+        return new DefaultConverter(this.getClass())
+        {
+            @ConvertMethod(from = String.class, to = BufferAllocator.class)
+            public BufferAllocator stringToBytes(String value)
+            {
+                try
+                {
+                    return (BufferAllocator) Class.forName(value).newInstance();
+                }
+                catch (Exception e)
+                {
+                    throw new UnexpectedRuntimeException("value "+value+" can't be converted to BufferAllocator",e);
+                }
+            }
+        };
     }
 }
